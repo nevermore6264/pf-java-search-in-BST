af07f886e1f4e6b656ba3df1bcfc83576b13ffb9
nevermore6264 <nevermore6264@gmail.com>
2018-04-06 11:48:41 +0700

    [Bài tập] Thao tác tìm kiếm, duyệt thứ tự pre, pos, xóa trong BST

--- src/hieutt/SearchInBST.java
+++ src/hieutt/SearchInBST.java
@@ -12,7 +12,7 @@ public class SearchInBST {
     }
 
     //Tạo root
-    BinarySearchTree.Node root;
+    Node root;
 
     public Node search(Node root, int key) {
         if (root == null || root.key == key) {
@@ -38,6 +38,36 @@ public class SearchInBST {
             preOrderTraversal(root.left);
             preOrderTraversal(root.right);
         }
+    }
+
+    public void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, int key) {
+        if (root == null) return root;
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
     }
 
 }
